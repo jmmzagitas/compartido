0530da0575a2a8a35c73bc4ff31a9141d7373341
jmmzagitas <123980434+jmmzagitas@users.noreply.github.com>
2023-04-28 11:26:16 -0500

    Add files via upload

---
+++ PruebaFormRecognizer.py
@@ -0,0 +1,71 @@
+"""
+This code sample shows Prebuilt Layout operations with the Azure Form Recognizer client library. 
+The async versions of the samples require Python 3.6 or later.
+
+To learn more, please visit the documentation - Quickstart: Form Recognizer Python client library SDKs
+https://docs.microsoft.com/en-us/azure/applied-ai-services/form-recognizer/quickstarts/try-v3-python-sdk
+"""
+
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.formrecognizer import DocumentAnalysisClient
+
+"""
+Remember to remove the key from your code when you're done, and never post it publicly. For production, use
+secure methods to store and access your credentials. For more information, see 
+https://docs.microsoft.com/en-us/azure/cognitive-services/cognitive-services-security?tabs=command-line%2Ccsharp#environment-variables-and-application-configuration
+"""
+endpoint = "https://ipesa001cognitiveservice.cognitiveservices.azure.com/"
+key = "f458155103654075ac14e3b705128688"
+
+# sample document
+formUrl = "https://github.com/jmmzagitas/compartido/raw/cda0fec6340f0b37e020ede60982d93e00ead086/pdf%20bcp%20miami%20%20euros%2027.04.2023.pdf"
+
+document_analysis_client = DocumentAnalysisClient(
+    endpoint=endpoint, credential=AzureKeyCredential(key)
+)
+    
+poller = document_analysis_client.begin_analyze_document_from_url("prebuilt-layout", formUrl)
+result = poller.result()
+
+for idx, style in enumerate(result.styles):
+    print(
+        "Document contains {} content".format(
+         "handwritten" if style.is_handwritten else "no handwritten"
+        )
+    )
+
+for page in result.pages:
+    for line_idx, line in enumerate(page.lines):
+        print(
+         "...Line # {} has text content '{}'".format(
+        line_idx,
+        line.content.encode("utf-8")
+        )
+    )
+
+    for selection_mark in page.selection_marks:
+        print(
+         "...Selection mark is '{}' and has a confidence of {}".format(
+         selection_mark.state,
+         selection_mark.confidence
+         )
+    )
+
+for table_idx, table in enumerate(result.tables):
+    print(
+        "Table # {} has {} rows and {} columns".format(
+        table_idx, table.row_count, table.column_count
+        )
+    )
+        
+    for cell in table.cells:
+        print(
+            "...Cell[{}][{}] has content '{}'".format(
+            cell.row_index,
+            cell.column_index,
+            cell.content.encode("utf-8"),
+            )
+        )
+
+print("----------------------------------------")
+
